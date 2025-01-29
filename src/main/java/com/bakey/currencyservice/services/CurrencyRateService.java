127bdd5887fd3ca275e6a89635731b3ce3f22038
Bakey <klou999d@gmail.com>
2025-01-30 00:00:33 +0300

    update CurrencyRateService

--- src/main/java/com/bakey/currencyservice/services/CurrencyRateService.java
+++ src/main/java/com/bakey/currencyservice/services/CurrencyRateService.java
@@ -1,5 +1,6 @@
 package com.bakey.currencyservice.services;
 
+import com.bakey.currencyservice.CurrencyRateClient;
 import com.bakey.currencyservice.models.CurrencyRate;
 import com.bakey.currencyservice.repositories.CurrencyRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,19 @@ public class CurrencyRateService {
     @Autowired
     private CurrencyRateRepository currencyRateRepository;
 
+    @Autowired
+    private CurrencyRateClient currencyRateClient;
+
     public CurrencyRate saveCurrencyRate(CurrencyRate currencyRate) {
         return currencyRateRepository.save(currencyRate);
     }
 
     public List<CurrencyRate> getAllCurrencyRates() {
         return currencyRateRepository.findAll();
+    }
+
+    public CurrencyRate fetchAndSaveCurrencyRate(String fromCurrency, String toCurrency) {
+        CurrencyRate currencyRate = currencyRateClient.getCurrencyRate(fromCurrency, toCurrency);
+        return saveCurrencyRate(currencyRate);
     }
 }
